8875a0aff7382f208417675e7559db14d1d25cf6
unknown <uditnag@cmail.carleton.ca>
2024-07-20 15:26:23 -0400

    changed image to popup inside gallery

--- public/js/artWorks.js
+++ public/js/artWorks.js
@@ -9,22 +9,45 @@
 
 
 
-// function expandImage(){
+function expandImage(){
     
-//     console.log("Clicked element ID:", this.id);
+  document.getElementById("image-window").src = '';
 
-//     this.style.width = '700px';
-//     this.style.margin = '0% 20%';
+  let imageID = this.id;
+
+
+  let displayFrame = document.getElementById("image-window");
   
-// }
+  displayFrame.src = "/artworks/" + imageID;
+
+  //   let req = new XMLHttpRequest();
+  //   req.onreadystatechange = function() {
+	// 	if(this.readyState==4 && this.status==200){
+			
+  //           let displayFrame = document.getElementById("display-box");
+            
+  //           displayFrame.innerHTML='';
+  //           displayFrame.innerHTML = this.responseText;
+            
+
+	// 	}
+
+	// }
+
+  //   req.open("GET", "/artworks/" + imageID);
+  //   req.setRequestHeader('Content-Type', 'text/html');
+  //   req.send();
+   
+  
+}
 
 
 
-// const imageContainers = document.getElementsByClassName("img-box");
+const imageContainers = document.getElementsByClassName("img-box");
 
 
-// for (const container of imageContainers) {
-//     container.addEventListener("click", expandImage); // Or any event you want
+for (const container of imageContainers) {
+    container.addEventListener("click", expandImage); // Or any event you want
     
-//     console.log(container.id);
-//   }
+    console.log(container.id);
+  }
